2c1c681328ac15b389824daa27ea70cbe28f3756
BeckSM64 <becksm64@gmail.com>
2025-11-11 16:56:12 -0500

    :art: make the pieces bigger

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -11,4 +11,5 @@ Piece::Piece(PieceColor c, const sf::Vector2i& pos, const std::string& texturePa
     sprite.setTexture(texture);
     sprite.setOrigin(texture.getSize().x / 2.f, texture.getSize().y / 2.f);
     sprite.setPosition(static_cast<float>(pos.x), static_cast<float>(pos.y));
+    sprite.setScale(2.0f, 2.0f);
 }
